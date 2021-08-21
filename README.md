570356eef477048f4cbcd485744bd32dc184d3d1
Alejandro De La Lama <delalama.7@gmail.com>
2021-08-21 14:55:57 -0500

    added other inquirer questions and saved values

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# ewoinv
-## Description
-lqwkfqlwfmq
+# TITLE
+![](https://img.shields.io/badge/license-MIT-blue?style=flat-square)
+## Description:
+ Enter project description here.
+## Usage:
+ Input project usage.
+
+## License:
+This project is convered under the MIT.
+## Dependencies:
+npm install
+## Testing:
+npm test
+
+## Contact:
+If you have any questions or want to contact me, you can reach me at my GitHub: [adelalama](https://github.com/adelalama), or delalama.7@gmail.com by mail.
--- index.js
+++ index.js
@@ -2,46 +2,29 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
-inquirer 
-    .prompt([
-        {
-          type: "input",
-          message: "What is the title of your project?",
-          name: "title"
-        },
-        {
-          type: "input",
-          message: "What is a short description of your project?",
-          name: "description" 
-        }
-    ])
-    .then((res)=>{
-      createReadMe(res);
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-/*const answers = [
-  {
-    type: 'input',
-    name: 'email',
-    message: 'Please write your email address?',
-  },
-  {
-    type: 'input',
-    name: 'title',
-    message: "Please write your project's name?",
-  },
-  {
-    type: 'input',
-    name: 'information',
-    message: 'Please write a short description of your project',
-  },
-  {
-    type: "list",
-    name: "license",
-    message: "Chose the appropriate license for this project: ",
-    choices: [
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "What is a short description of your project?",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "What is your e-mail address?",
+      name: "mail"
+    },
+    {
+
+      type: "list",
+      name: "license",
+      message: "Chose the appropriate license for this project: ",
+      choices: [
         "Apache",
         "Academic",
         "GNU",
@@ -49,84 +32,141 @@ inquirer
         "MIT",
         "Mozilla",
         "Open"
-    ]
-  },
-  {
-    type: 'input',
-    name: 'ghusername',
-    message: 'What is your GitHub username?',
-  },
-  {
-    type: 'input',
-    name: 'test',
-    message: 'Command to run tests(if there are any):',
-    default: 'npm test',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Command to install dependencies:',
-    default: 'npm i',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'How to use repo:',
-  },
-  {
-    type: 'input',
-    name: 'contributing',
-    message: 'How can the user contribute to the repo?',
-  },
-];
+      ]
 
-// function to add user input to generated readme file
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+    },
+    {
+      type: 'input',
+      message: 'Please enter your GitHub user:',
+      name: 'github'
+    },
+    {
+      type: 'input',
+      message: 'Command to run tests:',
+      default: 'npm test',
+      name: 'test'
+    },
+    {
+      type: 'input',
+      message: 'Command to install dependencies',
+      default: 'npm install',
+      name: 'dependencies'
+    },
+    {
+      type: 'input',
+      message: 'Enter the instrucctions to use the repo:',
+      name: 'usage'
+    }
+
+  ])
+  .then((res) => {
+    createReadMe(res);
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 
-
-// initialize function to ask for user input
-function init() {
-  inquirer.prompt(answers).then((inquirerResponses) => {
-    console.log('Generating README...');
-    writeToFile('README.md', generateReadme({ ...inquirerResponses }));
-  });
-}
-
-init();
-*/
-
-function createReadMe(input){
+function createReadMe(input) {
   let readMeTitle;
-  let readMeDesc; 
-  let filledReadMe = [];
-  const descriptionHead = "## Description";
+  let readMeDesc;
+  let readMeMail;
+  let readMeUsage;
+  let readMeLicense = input.license;
+  let readMeDependencies;
+  let readMeTest;
+  let readMeGitHub;
+  let readMecontact;
 
+  let filledReadMe = [];
+
+  const descriptionHead = "## Description:";
+  const contactHead = '## Contact:';
+  const usageHead = '## Usage:';
+  const dependenciesHead = '## Dependencies:';
+  const TestHead = '## Testing:';
+  const gitHubHead = '## GitHub:';
+  const licenseHead = '## License:'
+
+  //title
   if (input.title == '') {
     readmeTitle = '# TITLE';
-} else {
-    readmeTitle = `# ${input.title}`;
-}
-filledReadMe.push(readmeTitle);
-
-if (input.description == '') {
-  readmeDescription = `${descriptionHead}\n Enter project description here.`;
-} else {
-  readmeDescription = `${descriptionHead}\n${input.description}`;
-}
-filledReadMe.push(readmeDescription);
-
-const finalReadMe = filledReadMe.join('\n')
-
-
-fs.writeFile("./README.md", finalReadMe, (err) => {
-  if (err) {
-      throw err;
   } else {
-      console.log("README file successfully created!");
+    readmeTitle = `# ${input.title}`;
   }
-});
+  filledReadMe.push(readmeTitle);
+
+  //license badge
+  let badge = `![](https://img.shields.io/badge/license-${readMeLicense.replace(/ /g, "%20")}-blue?style=flat-square)`;
+  filledReadMe.push(badge)
+
+  //desc
+  if (input.description == '') {
+    readmeDesc = `${descriptionHead}\n Enter project description here.`;
+  } else {
+    readmeDesc = `${descriptionHead}\n${input.description}`;
+  }
+  filledReadMe.push(readmeDesc);
+
+
+  //mail
+  /*if (input.mail == '') {
+    readMeMail = `${emailHead}\n Enter e-mail here.`;
+  } else {
+    readMeMail = `${emailHead}\n${input.mail}`;
+  }
+  filledReadMe.push(readMeMail);
+  */
+  //usage
+  if (input.usage == '') {
+    readMeUsage = `${usageHead}\n Input project usage.`;
+  } else {
+    readMeUsage = `${usageHead}\n${input.usage}`;
+  }
+  filledReadMe.push(readMeUsage);
+
+  //license
+  readMeLicense = `\n${licenseHead}\nThis project is convered under the ${input.license}.`;
+  filledReadMe.push(readMeLicense)
+
+  //dependencies
+
+  if (input.dependencies == '') {
+    readMeDependencies = `${dependenciesHead}\n Enter dependencies:`;
+  } else {
+    readMeDependencies = `${dependenciesHead}\n${input.dependencies}`;
+  }
+  filledReadMe.push(readMeDependencies);
+
+  //tests
+
+  if (input.test == '') {
+    readMeTest = `${TestHead}\n Enter Test commands here:`;
+  } else {
+    readMeTest = `${TestHead}\n${input.test}`;
+  }
+  filledReadMe.push(readMeTest);
+
+  //contact
+  readmeContact = `\n${contactHead}\nIf you have any questions or want to contact me, you can reach me at my GitHub: [${input.github}](https://github.com/${input.github}), or ${input.mail} by mail.`;
+  filledReadMe.push(readmeContact);
+
+
+
+
+
+
+
+
+
+  const finalReadMe = filledReadMe.join('\n')
+
+  fs.writeFile("./README.md", finalReadMe, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("README file successfully created!");
+    }
+  });
 
 }
